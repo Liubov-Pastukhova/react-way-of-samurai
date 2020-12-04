5472f0babc16a6d514f8eab16d9415b78b64a2de
Liubov-Pastukhova <eukaryota2101@yandex.ru>
2020-12-04 15:52:45 +0400

    app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route} from 'react-router-dom';
 import './App.css';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import Newspage from './components/Newspage/Newspage';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import NavbarContainer from './components/Navbar/NavbarContainer';
+import FriendsContainer from './components/Friends/FriendsContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+import Login from './components/Login/Login';
 
-function App() {
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+      <HeaderContainer />
+      <NavbarContainer />
+      <div className='app-wrapper-content'>
+        <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
+        <Route path='/dialogs' render={() => <DialogsContainer />} />
+        <Route path='/newspage' render={() => <Newspage />} />
+        <Route path='/music' render={() => <Music />} />
+        <Route path='/users' render={() => <UsersContainer />} />
+        <Route path='/settings' render={() => <Settings />} />
+        <Route path='/friends' render={() => <FriendsContainer />} />
+        <Route path='/login' render={() => <Login />} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
